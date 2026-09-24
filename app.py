6b752b0fd8f2008119fad3ad55149f6f1ed35a72
saibabu2211 <saibabu.empcts@gmail.com>
2026-09-24 11:14:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import gradio as gr
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+load_dotenv()
+api=os.getenv('GEMEINI_API_KEY')
+
+client = genai.Client(api_key=userdata.get('GEMINI_API_KEY'))
+
+personalities = {
+  "Friendly": """/visual learning  You are a friendly, enthusiastic, and highly encouraging Study Assistant.
+  Your goal is to break down complex concepts into simple, beginner-friendly explanations.
+  Use analogies and real-world examples that beginners can relate to. Always ask a follow-up
+  question to check understanding.""",
+
+  "Academic": """You are a strictly academic, highly detailed, and professional university
+   Professor. Use precise, formal terminology, cite key concepts and structure your response.
+   Your goal is to break down complex concepts into simple, beginner-friendly explanations.
+   Use analogies and real-world examples that beginners can relate to. Always ask a follow-up question
+    to check understanding."""
+}
+
+def study_assistant(question,persona):
+  system_prompt = personalities[persona]
+  response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    config = types.GenerateContentConfig(
+        temperature=0.4,
+        max_output_tokens=5000,
+        system_instruction=system_prompt
+    ),
+    contents= question
+  )
+  return response.text
+demo = gr.Interface(
+  fn=study_assistant,
+  inputs=[
+      gr.Textbox(lines=4, placeholder="Ask a question...", label="Question"),
+      gr.Radio(choices=list(personalities.keys()), value="Friendly", label="Personality")
+  ],
+outputs=gr.Textbox(lines=10, label="Response"),
+title="Saibabu Study Assistant",
+description="Ask a question and get an answer from your AI study assistant with a chosen personality."
+)
+demo.launch(debug=True)
